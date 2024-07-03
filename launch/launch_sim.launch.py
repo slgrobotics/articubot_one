129befb42d40eab8ecac5cff56071c4d673b9205
Sergei <msghub@hotmail.com>
2024-07-02 19:11:55 -0500

    Update launch_sim.launch.py
    Starts but crashes. Added delayed start for some nodes

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,8 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
@@ -66,6 +69,36 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
+    ##################################################################
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{'robot_description': robot_description}, controller_params_file],
+        remappings=[('/diff_cont/odom','/odom')]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner],
+        )
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner],
+        )
+    )
+
+    ##################################################################
 
 
     # Code for delaying a node (I haven't tested how effective it is)
@@ -91,8 +124,12 @@ def generate_launch_description():
         rsp,
         joystick,
         twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
+        #diff_drive_spawner,
+        #joint_broad_spawner
     ])
+
