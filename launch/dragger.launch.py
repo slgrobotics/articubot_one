69d314464058717116992f6928d8f7938e1f1125
Sergei Grichine <msghub@hotmail.com>
2024-10-18 11:48:23 -0500

    Removed online_async_launch.py - using one from slam_toolbox with parameter 'slam_params_file'

--- launch/dragger.launch.py
+++ launch/dragger.launch.py
@@ -34,8 +34,7 @@ def generate_launch_description():
     slam_toolbox_params_file = os.path.join(package_path,'config','mapper_params_dragger.yaml')
 
     slam_toolbox = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(package_path,'launch','online_async_launch.py')]
-                #PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py')]
+                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py')]
                 ), launch_arguments={'use_sim_time': 'false', 'slam_params_file': slam_toolbox_params_file}.items()
     )
 
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -32,8 +32,7 @@ def generate_launch_description():
     slam_toolbox_params_file = os.path.join(package_path,'config','mapper_params_online_async.yaml')
 
     slam_toolbox = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(package_path,'launch','online_async_launch.py')]
-                #PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py')]
+                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py')]
                 ), launch_arguments={'use_sim_time': 'true', 'slam_params_file': slam_toolbox_params_file}.items()
     )
 
