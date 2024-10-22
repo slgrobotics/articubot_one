bb0fe4e062796f324c20bc1bc1f136b62f03bd4d
Sergei Grichine <msghub@hotmail.com>
2024-10-22 17:00:17 -0500

    Exploring twist_marker and joystick_relay

--- launch/twist_mux.launch.py
+++ launch/twist_mux.launch.py
@@ -28,6 +28,42 @@ def generate_launch_description():
         remappings=[('/cmd_vel_out','/diff_cont/cmd_vel')]
     )
 
+    # See https://www.youtube.com/watch?v=PN_AxCug5lg
+    #     https://answers.ros.org/question/370487/what-is-interactive_marker_twist_server-package/
+    #     https://github.com/ros-visualization/interactive_marker_twist_server/tree/humble-devel
+    #
+    # In robotics, a twist is a 6x1 column vector of linear and angular velocities. In ROS, this is implemented as a Twist message,
+    #  which you can see for yourself by entering the following in a terminal:
+    #     rosmsg show geometry_msgs/Twist
+    # An interactive marker is another ROS tool to interface with robots through rviz. You can drag around this marker,
+    #  and generally a robot will try to follow it. The server publishes twists (velocity commands) to get to the interactive marker pose (position command).
+    #     sudo apt install ros-${ROS_DISTRO}-interactive-marker-twist-server
+    #     ros2 launch interactive_marker_twist_server interactive_markers.launch.xml
+
+    twist_marker = Node(
+        package='twist_mux',
+        executable='twist_marker',
+        output='screen',
+        remappings={('/twist', '/diff_cont/cmd_vel')},
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'frame_id': 'base_link',
+            'scale': 1.0,
+            'vertical_position': 2.0}]
+    )
+
+    joystick_params_file = os.path.join(package_path,'config','joystick.yaml')
+
+    joystick_relay = Node(
+        package='twist_mux',
+        executable='joystick_relay.py',
+        output='screen',
+        remappings={('joy_vel_in', 'cmd_vel_joy'),
+                    ('joy_vel_out', 'joy_vel')},
+        parameters=[joystick_params_file, {'use_sim_time': use_sim_time}]
+    )
+
+    # this is how it should be, but "use_stamped" isn't working in that launch file:
     twist_mux_ = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("twist_mux"),'launch','twist_mux_launch.py')]
                 ), launch_arguments={
@@ -39,9 +75,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-        twist_mux       
+        twist_mux,
+        #twist_marker,
+        #joystick_relay
     ])
