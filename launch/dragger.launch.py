725a2a33a282f775a1e702cc63d995db1840b0c7
Sergei Grichine <msghub@hotmail.com>
2024-11-07 10:25:50 -0600

    WIP: working on Nav2, separate dragger_slam_launch

--- launch/dragger.launch.py
+++ launch/dragger.launch.py
@@ -36,11 +36,9 @@ def generate_launch_description():
                 ), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
-    slam_toolbox_params_file = os.path.join(package_path,'config','mapper_params_dragger.yaml')
-
     slam_toolbox = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory("slam_toolbox"),'launch','online_async_launch.py')]
-                ), launch_arguments={'use_sim_time': 'false', 'slam_params_file': slam_toolbox_params_file}.items()
+                PythonLaunchDescriptionSource([os.path.join(package_path,'launch','dragger_slam.launch.py')]
+                )
     )
 
     # You need to press "Startup" button in RViz when autostart=false
