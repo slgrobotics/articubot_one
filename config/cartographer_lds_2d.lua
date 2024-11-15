7fad505155d3e38a7b7657e4e9115c1296133fc6
Sergei Grichine <msghub@hotmail.com>
2024-11-14 22:45:51 -0600

    Update cartographer_lds_2d.lua
    WIP

--- config/cartographer_lds_2d.lua
+++ config/cartographer_lds_2d.lua
@@ -13,7 +13,7 @@
 -- limitations under the License.
 
 -- See turtlebot3_ws/src/turtlebot3/turtlebot3/turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
---     /opt/ros/jazzy/share/cartographer_ros/configuration_files/mir-100-mapping.lua
+--     /opt/ros/jazzy/share/cartographer_ros/configuration_files/mir-100-mapping.lua (MiR100 Mobile Robot)
 
 include "map_builder.lua"
 include "trajectory_builder.lua"
@@ -21,35 +21,50 @@ include "trajectory_builder.lua"
 options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
+
+-- See https://google-cartographer-ros.readthedocs.io/en/stable/configuration.html
   map_frame = "map",
-  tracking_frame = "base_link",
-  published_frame = "odom",
+  tracking_frame = "imu_link", -- "base_link", -- "laser_frame",
+  published_frame = "base_link", -- "base_link",
+
   odom_frame = "odom",
-  provide_odom_frame = false,
-  publish_frame_projected_to_2d = false,
+  provide_odom_frame = true,
+  publish_frame_projected_to_2d = true,
+
   use_odometry = true,
-  use_pose_extrapolator = true,
+  use_pose_extrapolator = false,
   use_nav_sat = true,
   use_landmarks = false,
+
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
-  lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
+
+  lookup_transform_timeout_sec = 0.3,
+  submap_publish_period_sec = 0.1,
+  pose_publish_period_sec = 5e-2, -- 20 Hz (5e-3 - 200 Hz)
+  trajectory_publish_period_sec = 30e-3, -- 30 ms = 33 Hz
+
   rangefinder_sampling_ratio = 1.0,
   odometry_sampling_ratio = 1.0,
   fixed_frame_pose_sampling_ratio = 1.0,
-  imu_sampling_ratio = 1.0,
+  imu_sampling_ratio = 0.1,
   landmarks_sampling_ratio = 1.0,
 }
+
+-- Override some default parammeters from files in /opt/ros/jazzy/share/cartographer/configuration_files
+--    map_builder.lua
+--    map_builder_server.lua
+--    pose_graph.lua
+--    trajectory_builder_2d.lua
+--    trajectory_builder_3d.lua
+--    trajectory_builder.lua
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER.collate_landmarks = false
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 2
-TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.submaps.num_range_data = 45
 
 -- more points
