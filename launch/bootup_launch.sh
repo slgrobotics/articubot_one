46513376a13bd00232f203c0a2b947553035f154
Sergei Grichine <msghub@hotmail.com>
2024-11-28 18:13:22 -0600

    Update bootup_launch.sh

--- launch/bootup_launch.sh
+++ launch/bootup_launch.sh
@@ -8,6 +8,9 @@ cd /home/ros/launch
 
 source /home/ros/robot_ws/install/setup.bash
 
+#aplay -D default:CARD=Set ~/wav/cat_meow.wav
+#aplay ~/wav/cat_meow.wav
+
 ros2 launch /home/ros/robot_ws/src/articubot_one/launch/dragger.launch.py
 #ros2 launch /home/ros/robot_ws/src/articubot_one/launch/launch_sim_nav.launch.py
 #ros2 launch /home/ros/robot_ws/src/articubot_one/launch/dragger_slam.launch.py
