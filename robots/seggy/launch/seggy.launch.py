2ae09c2364883292b47b9bb53f0794383475070a
Sergei Grichine <msghub@hotmail.com>
2025-10-31 13:20:32 -0500

    Nav2: pass nav2 params file to ComposableNodeContainer if  using composition

--- robots/seggy/launch/seggy.launch.py
+++ robots/seggy/launch/seggy.launch.py
@@ -82,9 +82,10 @@ def generate_launch_description():
     container_nav2 = ComposableNodeContainer(
         package='rclcpp_components',
         namespace=namespace,
-        executable='component_container_mt',
+        executable='component_container_mt', # _mt for multi-threaded
         name='nav2_container',
         composable_node_descriptions=[],
+        parameters=[nav2_params_file],   # must be passed here - see https://github.com/ros-navigation/navigation2/issues/4011
         output='screen'
     )
 
@@ -97,7 +98,7 @@ def generate_launch_description():
                                      'odom_topic': 'odometry/local',
                                      #'use_respawn': 'true',
                                      'autostart' : 'true',
-                                     'params_file' : nav2_params_file }.items()
+                                     'params_file' : nav2_params_file }.items() # pass nav2 params file if not using composition
     )
 
     controllers_params_file = os.path.join(robot_path,'config','controllers.yaml')
