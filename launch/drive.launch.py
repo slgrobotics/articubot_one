d9d25bb70be288d1d1cbcd94d38855a40007cadc
Sergei Grichine <msghub@hotmail.com>
2025-11-12 11:24:20 -0600

    Create drive.launch.py
    generic / common launch file for diff drive rovers

---
+++ launch/drive.launch.py
@@ -0,0 +1,129 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription, TimerAction, GroupAction, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessStart
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    # Accept namespace from parent launch or use empty default
+    namespace = LaunchConfiguration('namespace', default='')
+
+    package_name='articubot_one' #<--- CHANGE ME
+
+    # Robot specific files reside under "robots" directory - sim, dragger, plucky, seggy, create1...
+    robot_model = LaunchConfiguration('robot_model', default='')
+
+    # Build substitution-based paths so robot_model can be used at launch-time
+    robot_path_sub = PathJoinSubstitution([
+        FindPackageShare('articubot_one'), 'robots', robot_model
+    ])
+    
+    controllers_params_file_sub = PathJoinSubstitution([
+        FindPackageShare('articubot_one'), 'robots', robot_model, 'config', 'controllers.yaml'
+    ])
+
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # Include twist_mux for command velocity arbitration
+    twist_mux = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name),'launch','twist_mux.launch.py')]
+                ), launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        namespace=namespace,
+        executable="ros2_control_node",
+        parameters=[controllers_params_file_sub],
+        output="screen"
+    )
+
+    delayed_controller_manager = TimerAction(period=5.0, actions=[controller_manager])
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        namespace=namespace,
+        executable="spawner",
+        arguments=["joint_broad"],
+        output="screen"
+    )
+
+    battery_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        namespace=namespace,
+        executable="spawner",
+        arguments=["battery_state_broadcaster", "--controller-ros-args", "--remap battery_state_broadcaster/battery_state:=battery/battery_state"],
+        output="screen"
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        namespace=namespace,
+        executable="spawner",
+        arguments=["diff_cont", "--controller-ros-args", "--remap /tf:=diff_cont/tf"], # isolate TFs, if published.
+        output="screen"
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner]
+        )
+    )
+
+    delayed_battery_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[battery_state_broadcaster_spawner]
+        )
+    )
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=joint_broad_spawner,
+            on_start=[diff_drive_spawner]
+        )
+    )
+
+    drive_include = GroupAction(
+        actions=[
+            twist_mux,
+            delayed_controller_manager,
+            delayed_diff_drive_spawner,
+            delayed_joint_broad_spawner,
+            delayed_battery_state_broadcaster_spawner
+        ]
+    )
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace for drive nodes'),
+
+        DeclareLaunchArgument(
+            'robot_model',
+            default_value='',
+            description='Robot model (e.g., seggy, plucky, dragger)'),
+
+        LogInfo(msg='============ starting ROBOT DRIVE  use_sim_time:'),
+        LogInfo(msg=use_sim_time),
+        LogInfo(msg=robot_model_path),
+
+        drive_include
+    ])
