5ad9c03c368994d5238d792a16ee90f81425026a
Sergei Grichine <msghub@hotmail.com>
2025-11-12 10:44:37 -0600

    Separated Seggy's sensors and localizers into their own launch file

--- robots/seggy/launch/seggy.launch.py
+++ robots/seggy/launch/seggy.launch.py
@@ -14,7 +14,8 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    namespace=''
+    # Make namespace overridable at runtime
+    namespace = LaunchConfiguration('namespace', default='')
 
     package_name='articubot_one' #<--- CHANGE ME
 
@@ -41,41 +42,6 @@ def generate_launch_description():
                 ), launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
-    slam_toolbox = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(robot_path,'launch','seggy_slam_toolbox.launch.py')]
-                ), launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-
-    cartographer = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(robot_path,'launch','cartographer.launch.py')]
-                ), launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-
-    # Map server is convenient when used with GPS and an empty map, for obstacle avoidance.
-    #map_yaml_file = os.path.join(package_path,'assets','maps','empty_map.yaml')   # this is default anyway
-    map_yaml_file = '/opt/ros/jazzy/share/nav2_bringup/maps/warehouse.yaml'
-
-    map_server = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(package_path,'launch','map_server.launch.py')]
-                ), launch_arguments={'use_sim_time': use_sim_time}.items()       # empty_map - default
-                #), launch_arguments={'map': map_yaml_file, 'use_sim_time': use_sim_time}.items() # warehouse
-    )
-
-    # odom_localizer is needed for slam_toolbox, providing "a valid transform from your configured odom_frame to base_frame"
-    # also, produces odom_topic: /odometry/local which can be used by Nav2
-    # see https://github.com/SteveMacenski/slam_toolbox?tab=readme-ov-file#api
-    # see mapper_params.yaml
-    odom_localizer = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(package_path,'launch','ekf_odom.launch.py')]
-                ), launch_arguments={'use_sim_time': use_sim_time, 'robot_model' : robot_model}.items()
-    )
-
-    # alternative to odom_localizer for slam_toolbox
-    tf_localizer = Node(package = "tf2_ros", 
-                    executable = "static_transform_publisher",
-                    arguments = ["0", "0", "0", "0", "0", "0", "odom", "base_link"]
-    )
-    
     nav2_params_file = os.path.join(robot_path,'config','nav2_params.yaml')
 
     # Define the ComposableNodeContainer for Nav2 composition:
@@ -158,71 +124,6 @@ def generate_launch_description():
         )
     )
 
-    ldlidar_node = Node(
-        package='ldlidar_sl_ros2',
-        namespace=namespace,
-        executable='ldlidar_sl_ros2_node',
-        name='ldlidar_publisher_ld14',
-        output='screen',
-        respawn=True,
-        respawn_delay=10,
-        parameters=[
-          {'product_name': 'LDLiDAR_LD14'},
-          {'laser_scan_topic_name': 'scan'},
-          {'point_cloud_2d_topic_name': 'pointcloud2d'},
-          {'frame_id': 'laser_frame'},
-          {'port_name': '/dev/ttyUSBLDR'},
-          {'serial_baudrate' : 115200},
-          {'laser_scan_dir': True},
-          # Seggy has vertical bar behind the LiDAR, so we crop angles around 180 degrees:
-          {'enable_angle_crop_func': True},
-          {'angle_crop_min': 170.0},
-          {'angle_crop_max': 190.0}
-        ]
-    )
-
-    mpu9250driver_node = Node(
-        package="mpu9250",
-        namespace=namespace,
-        executable="mpu9250",
-        name="mpu9250",
-        output='screen',
-        respawn=True,
-        respawn_delay=4,
-        emulate_tty=True,
-        parameters=[
-          {
-              #"print" : True,
-              "frequency" : 60,
-              "i2c_address" : 0x68,
-              "i2c_port" : 1,
-              "frame_id" : "imu_link",
-              "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
-              "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
-              "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
-              "magnetometer_scale": [1.0, 1.0, 1.0],
-              #"magnetometer_bias": [1.3345253592582676, 2.6689567513691685, -2.5294210260199957],
-              #"magnetometer_bias": [1.335, 4.0, 1.0],
-              #"magnetometer_bias": [1.335, 3.8, -2.5294210260199957],
-              "magnetometer_bias": [1.335, 4.0, -2.53],
-              "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
-                                        -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
-                                         0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
-          }
-        ],
-        remappings=[("imu", "imu/data")]
-    )
-
-    face_gesture_sensor = Node(
-        package="face_gesture_sensor",
-        namespace=namespace,
-        executable="fgs_node",
-        name="face_gesture_sensor",
-        output='screen',
-        respawn=True,
-        respawn_delay=4,
-    )
-
     drive_include = GroupAction(
         actions=[
             twist_mux,
@@ -233,26 +134,17 @@ def generate_launch_description():
         ]
     )
 
-    sensors_include = GroupAction(
-        actions=[
-            ldlidar_node,
-            mpu9250driver_node,
-            face_gesture_sensor
-        ]
+    # Include separate sensors launch for better modularity
+    sensors_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(robot_path,'launch','seggy.sensors.launch.py')]),
+        launch_arguments={'use_sim_time': use_sim_time, 'namespace': namespace}.items()
     )
 
-    localizers_include = GroupAction(
-        actions=[
-            LogInfo(msg='============ starting LOCALIZERS ==============='),
-            odom_localizer, # needed for slam_toolbox. cartographer doesn't need it when cartographer.launch.py uses direct mapping
-            #tf_localizer,
-            # use either map_server, OR cartographer OR slam_toolbox, as they are all mappers
-            #cartographer, # localization via LIDAR
-            slam_toolbox, # localization via LIDAR
-        ]
+    # Include separate localizers launch for better modularity
+    localizers_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(robot_path,'launch','seggy.localizers.launch.py')]),
+        launch_arguments={'use_sim_time': use_sim_time, 'namespace': namespace, 'robot_model': robot_model}.items()
     )
-
-    delayed_loc = TimerAction(period=10.0, actions=[localizers_include])
 
     delayed_nav = TimerAction(period=20.0, actions=[nav2])
 
@@ -264,11 +156,16 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace for seggy nodes'),
+
         rsp,
         # joystick,
         drive_include,
         sensors_include,
-        delayed_loc,
+        localizers_include,
         container_nav2,  # Add the container to the launch description, if 'use_composition': 'True' is set
         delayed_nav
     ])
---
+++ robots/seggy/launch/seggy.sensors.launch.py
@@ -0,0 +1,90 @@
+import os
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    # Keep interface compatible with being included from seggy.launch.py
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    # Allow the including launch file to set a namespace via a launch-argument
+    namespace = LaunchConfiguration('namespace', default='')
+
+    # Lidar node - https://github.com/slgrobotics/robots_bringup/blob/main/Docs/Sensors/LD14.md
+    ldlidar_node = Node(
+        package='ldlidar_sl_ros2',
+        namespace=namespace,
+        executable='ldlidar_sl_ros2_node',
+        name='ldlidar_publisher_ld14',
+        output='screen',
+        respawn=True,
+        respawn_delay=10,
+        parameters=[
+          {'use_sim_time': use_sim_time},
+          {'product_name': 'LDLiDAR_LD14'},
+          {'laser_scan_topic_name': 'scan'},
+          {'point_cloud_2d_topic_name': 'pointcloud2d'},
+          {'frame_id': 'laser_frame'},
+          {'port_name': '/dev/ttyUSBLDR'},
+          {'serial_baudrate' : 115200},
+          {'laser_scan_dir': True},
+          # Seggy has vertical bar behind the LiDAR, so we crop angles around 180 degrees:
+          {'enable_angle_crop_func': True},
+          {'angle_crop_min': 170.0},
+          {'angle_crop_max': 190.0}
+        ]
+    )
+
+    # IMU node - https://github.com/slgrobotics/robots_bringup/blob/main/Docs/Sensors/MPU9250.md
+    mpu9250driver_node = Node(
+        package="mpu9250",
+        namespace=namespace,
+        executable="mpu9250",
+        name="mpu9250",
+        output='screen',
+        respawn=True,
+        respawn_delay=4,
+        emulate_tty=True,
+        parameters=[
+          {'use_sim_time': use_sim_time},
+          {
+              #"print" : True,
+              "frequency" : 60,
+              "i2c_address" : 0x68,
+              "i2c_port" : 1,
+              "frame_id" : "imu_link",
+              "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
+              "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
+              "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
+              "magnetometer_scale": [1.0, 1.0, 1.0],
+              #"magnetometer_bias": [1.3345253592582676, 2.6689567513691685, -2.5294210260199957],
+              #"magnetometer_bias": [1.335, 4.0, 1.0],
+              #"magnetometer_bias": [1.335, 3.8, -2.5294210260199957],
+              "magnetometer_bias": [1.335, 4.0, -2.53],
+              "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
+                                        -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
+                                         0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
+          }
+        ],
+        remappings=[("imu", "imu/data")]
+    )
+
+    # Face gesture sensor - https://github.com/slgrobotics/robots_bringup/blob/main/Docs/Sensors/FaceGesture.md
+    face_gesture_sensor = Node(
+        package="face_gesture_sensor",
+        namespace=namespace,
+        executable="fgs_node",
+        name="face_gesture_sensor",
+        output='screen',
+        respawn=True,
+        respawn_delay=4,
+        parameters=[{'use_sim_time': use_sim_time}],
+    )
+
+    return LaunchDescription([
+        ldlidar_node,
+        mpu9250driver_node,
+        face_gesture_sensor
+    ])
