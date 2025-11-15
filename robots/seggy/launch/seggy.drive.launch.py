cb7b8651fc561118c25ad69c40ed41ab6ea32093
Sergei Grichine <msghub@hotmail.com>
2025-11-15 12:10:07 -0600

    Cleaned up drive launch files for Seggy and Turtle

--- robots/seggy/launch/seggy.drive.launch.py
+++ robots/seggy/launch/seggy.drive.launch.py
@@ -1,16 +1,15 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 #
 # Generate launch description for Seggy robot drive system
 #
 # This launch file includes the generic "drive.launch.py" with appropriate
-# parameters for Seggy robot.
+# parameters for the Seggy robot.
 #
 
 def generate_launch_description():
@@ -22,13 +21,15 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     robot_model = LaunchConfiguration('robot_model', default='')
 
-    package_path = get_package_share_directory(package_name)
+    # Seggy uses common differential drive system, and we have a "generic" drive launch for that.
+    # Include the "generic" drive launch and pass through all arguments.
+    # Use the non-sim drive when "use_sim_time" is false
+    drive_launch_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'drive.launch.py'
+    ])
 
-    # Include the main / common drive launch and pass through all arguments.
-    # Use the non-sim drive when use_sim_time is false, and the sim-specific
-    # drive when use_sim_time is true.
     drive_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(package_path, 'launch', 'drive.launch.py')),
+        PythonLaunchDescriptionSource(drive_launch_path),
         launch_arguments={
             'namespace': namespace,
             'use_sim_time': use_sim_time,
@@ -37,8 +38,14 @@ def generate_launch_description():
         condition=UnlessCondition(use_sim_time)
     )
 
+    # Use the sim-specific generic drive launch when "use_sim_time" is true.
+    # It will pick URDF and controller config based on robot_model argument.
+    drive_sim_launch_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'drive_sim.launch.py'
+    ])  
+
     drive_sim_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(package_path, 'launch', 'drive_sim.launch.py')),
+        PythonLaunchDescriptionSource(drive_sim_launch_path),
         launch_arguments={
             'namespace': namespace,
             'use_sim_time': use_sim_time,
@@ -50,6 +57,13 @@ def generate_launch_description():
     # add any robot-specific drive launch operations here if needed
 
     return LaunchDescription([
+
+        # not needed for files not meant to run directly, parent declares and passes arguments,
+        # but for consistency and clarity, adding declarations is harmless:
+        DeclareLaunchArgument('namespace', default_value=''),
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument('robot_model', default_value=''),
+
         drive_launch,
         drive_sim_launch
     ])
--- robots/turtle/launch/turtle.drive.launch.py
+++ robots/turtle/launch/turtle.drive.launch.py
@@ -1,17 +1,16 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
-import os
 
 #
 # Generate launch description for Turtle robot drive system
 #
-# This launch file includes the generic "drive.launch.py" with appropriate
-# parameters for turtle robot.
+# This launch file includes the generic "drive_sim.launch.py" with appropriate
+# parameters for turtle robot in simulation.
 #
 
 def generate_launch_description():
@@ -23,10 +22,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     robot_model = LaunchConfiguration('robot_model', default='')
 
-    package_path = get_package_share_directory(package_name)
-
     # For real robot, include the Roomba Create 1 specific drive launch and pass through all arguments.
-    # Use the non-sim drive when use_sim_time is false
+    # Use the non-sim drive when "use_sim_time" is false
     drive_launch = Node(
         package='create_driver',
         namespace=namespace,
@@ -36,7 +33,7 @@ def generate_launch_description():
         respawn=True,
         respawn_delay=4,
         parameters=[{
-            'robot_model': 'CREATE_1',
+            'robot_model': 'CREATE_1', # not the same as robot_model argument
             'dev': '/dev/ttyUSB0',
             'baud': 57600,
             'base_frame': 'base_link',
@@ -52,9 +49,14 @@ def generate_launch_description():
         condition=UnlessCondition(use_sim_time)
     )
 
-    # Use the sim-specific generic drive launch when use_sim_time is true.
+    # Use the sim-specific generic drive launch when "use_sim_time" is true.
+    # It will pick URDF and controller config based on robot_model argument.
+    drive_sim_launch_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'drive_sim.launch.py'
+    ])  
+
     drive_sim_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(package_path, 'launch', 'drive_sim.launch.py')),
+        PythonLaunchDescriptionSource(drive_sim_launch_path),
         launch_arguments={
             'namespace': namespace,
             'use_sim_time': use_sim_time,
@@ -66,6 +68,9 @@ def generate_launch_description():
     # add any robot-specific drive launch operations here if needed
 
     return LaunchDescription([
+
+        # not needed for files not meant to run directly, parent declares and passes arguments,
+        # but for consistency and clarity, adding declarations is harmless:
         DeclareLaunchArgument('namespace', default_value=''),
         DeclareLaunchArgument('use_sim_time', default_value='false'),
         DeclareLaunchArgument('robot_model', default_value=''),
