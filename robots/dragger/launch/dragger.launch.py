bc19df66efb44073c5a7712d3dd92ff151baa19c
Sergei Grichine <msghub@hotmail.com>
2025-11-15 19:17:25 -0600

    Refactored Dragger launch files (similar to Seggy's)

--- robots/dragger/launch/dragger.launch.py
+++ robots/dragger/launch/dragger.launch.py
@@ -1,19 +1,30 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction, GroupAction, LogInfo, RegisterEventHandler
+from launch.conditions import UnlessCondition
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
-from launch.event_handlers import OnProcessStart
-from launch_ros.actions import ComposableNodeContainer, Node
+
+#
+# Generate launch description for Dragger robot
+#
+# if launched with "use_sim_time=true", will be running in Gazebo simulation
+#
+# On the robot's Raspberry Pi:
+#
+#     ros2 launch articubot_one dragger.launch.py
+#
+# On the Desktop in simulation:
+#
+#     ros2 launch articubot_one dragger.launch.py use_sim_time:=true
+#
 
 def generate_launch_description():
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    namespace=''
-
     package_name='articubot_one'
+
+    # Make namespace overridable at runtime
+    namespace = LaunchConfiguration('namespace', default='')
 
     robot_model='dragger'
 
@@ -23,272 +34,40 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'rsp.launch.py'])
         ),
-        launch_arguments={'use_sim_time': use_sim_time, 'robot_model': robot_model}.items()
+        launch_arguments={'namespace': namespace, 'use_sim_time': use_sim_time, 'robot_model': robot_model}.items()
     )
 
-    twist_mux = IncludeLaunchDescription(
+    # Include separate launch files for better modularity
+
+    drive_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'twist_mux.launch.py'])
+            PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'launch', 'dragger.drive.launch.py'])
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={'namespace': namespace, 'use_sim_time': use_sim_time, 'robot_model': robot_model}.items()
     )
 
-    slam_toolbox = IncludeLaunchDescription(
+    sensors_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'launch', 'dragger_slam_toolbox.launch.py'])
+            PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'launch', 'dragger.sensors.launch.py'])
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
+        launch_arguments={'namespace': namespace, 'use_sim_time': use_sim_time, 'robot_model': robot_model}.items(),
+        condition=UnlessCondition(use_sim_time) # only for real robot, not Gazebo simulation
     )
 
-    #map_yaml_file = PathJoinSubstitution([FindPackageShare(package_name), 'assets', 'maps', 'empty_map.yaml'])   # this is default anyway
-    map_yaml_file = '/opt/ros/jazzy/share/nav2_bringup/maps/warehouse.yaml'
-
-    map_server = IncludeLaunchDescription(
+    localizers_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'map_server.launch.py'])
+            PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'launch', 'dragger.localizers.launch.py'])
         ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()       # empty_map - default
-        #), launch_arguments={'map': map_yaml_file, 'use_sim_time': use_sim_time}.items() # warehouse
+        launch_arguments={'namespace': namespace, 'use_sim_time': use_sim_time, 'robot_model': robot_model}.items()
     )
 
-    nav2_params_file = PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'config', 'nav2_params.yaml'])
-
-    # Define the ComposableNodeContainer for Nav2 composition:
-    container_nav2 = ComposableNodeContainer(
-        package='rclcpp_components',
-        namespace=namespace,
-        executable='component_container_mt', # _mt for multi-threaded
-        name='nav2_container',
-        composable_node_descriptions=[], # leave empty, as we are using nav2_launch.py to load components
-        parameters=[nav2_params_file],   # must be passed here - see https://github.com/ros-navigation/navigation2/issues/4011
-        output='screen'
-    )
-
-    # You need to press "Startup" button in RViz when autostart=false
-    nav2 = IncludeLaunchDescription(
+    # Include generic navigation stack launch, it will pick up Dragger's "nav2_params.yaml"
+    navigation_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'navigation_launch.py'])
+            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'navigation.launch.py'])
         ),
-        launch_arguments={'use_sim_time': use_sim_time,
-                          'use_composition': 'True',
-                          'container_name': 'nav2_container',
-                          'odom_topic': 'odometry/local',
-                          #'use_respawn': 'true',
-                          'autostart': 'true',
-                          'params_file': nav2_params_file}.items()  # pass nav2 params file if not using composition
+        launch_arguments={'namespace': namespace, 'use_sim_time': use_sim_time, 'robot_model': robot_model, 'delay': '20.0'}.items()
     )
-
-    controllers_params_file = PathJoinSubstitution([FindPackageShare(package_name), 'robots', robot_model, 'config', 'controllers.yaml'])
-
-    controller_manager = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="ros2_control_node",
-        parameters=[controllers_params_file],
-        remappings=[
-            ('sonar_broadcaster_F_L/range', 'sonar_F_L'),
-            ('sonar_broadcaster_F_R/range', 'sonar_F_R'),
-            ('sonar_broadcaster_B_L/range', 'sonar_B_L'),
-            ('sonar_broadcaster_B_R/range', 'sonar_B_R')
-        ]
-    )
-
-    delayed_controller_manager = TimerAction(period=5.0, actions=[controller_manager])
-
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["joint_broad"],
-        output="screen"
-    )
-
-    battery_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["battery_state_broadcaster", "--controller-ros-args", "--remap battery_state_broadcaster/battery_state:=battery/battery_state"],
-        output="screen"
-    )
-
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["diff_cont", "--controller-ros-args", "--remap /tf:=diff_cont/tf"], # isolate TFs, if published.
-        output="screen"
-    )
-
-    sonar_f_l_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["sonar_broadcaster_F_L"]
-    )
-
-    sonar_f_r_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["sonar_broadcaster_F_R"]
-    )
-
-    sonar_b_l_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["sonar_broadcaster_B_L"]
-    )
-
-    sonar_b_r_spawner = Node(
-        package="controller_manager",
-        namespace=namespace,
-        executable="spawner",
-        arguments=["sonar_broadcaster_B_R"]
-    )
-
-    delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[joint_broad_spawner]
-        )
-    )
-
-    delayed_battery_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=controller_manager,
-            on_start=[battery_state_broadcaster_spawner]
-        )
-    )
-
-    delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=joint_broad_spawner,
-            on_start=[diff_drive_spawner]
-        )
-    )
-
-    delayed_sonars_spawner = RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=diff_drive_spawner,
-            on_start=[sonar_f_l_spawner, sonar_f_r_spawner, sonar_b_l_spawner, sonar_b_r_spawner]
-        )
-    )
-
-    ldlidar_node = Node(
-        package='ldlidar_sl_ros2',
-        namespace=namespace,
-        executable='ldlidar_sl_ros2_node',
-        name='ldlidar_publisher_ld14',
-        output='screen',
-        respawn=True,
-        respawn_delay=10,
-        parameters=[
-          {'product_name': 'LDLiDAR_LD14P'},  # LDLiDAR_LD14P setting works for LD-19P LIDAR
-          {'laser_scan_topic_name': 'scan'},
-          {'point_cloud_2d_topic_name': 'pointcloud2d'},
-          {'frame_id': 'laser_frame'},
-          {'port_name': '/dev/ttyUSBLDR'},
-          {'serial_baudrate' : 230400}, # LD-19P has 230400 baud rate
-          {'laser_scan_dir': True},
-          {'enable_angle_crop_func': False},
-          {'angle_crop_min': 135.0},
-          {'angle_crop_max': 225.0},
-          {'min_intensity': 45},
-          {'do_filtering': False},
-          {'do_triplets': False}
-        ]
-    )
-
-    mpu9250driver_node = Node(
-        package="mpu9250",
-        namespace=namespace,
-        executable="mpu9250",
-        name="mpu9250",
-        output='screen',
-        respawn=True,
-        respawn_delay=4,
-        emulate_tty=True,
-        parameters=[
-          {
-              #"print" : True,
-              "frequency" : 60,
-              "i2c_address" : 0x68,
-              "i2c_port" : 1,
-              "frame_id" : "imu_link",
-              "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
-              "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
-              "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
-              "magnetometer_scale": [1.0, 1.0, 1.0],
-              #"magnetometer_bias": [1.3345253592582676, 2.6689567513691685, -2.5294210260199957],
-              #"magnetometer_bias": [1.335, 4.0, 1.0],
-              #"magnetometer_bias": [1.335, 3.8, -2.5294210260199957],
-              "magnetometer_bias": [1.335, 4.0, -2.53],
-              "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
-                                        -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
-                                         0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
-          }
-        ],
-        remappings=[("imu", "imu/data")]
-    )
-
-    gps_node = Node(
-        package='nmea_navsat_driver',
-        executable='nmea_serial_driver',
-        output='screen',
-        respawn=True,
-        respawn_delay=10,
-        parameters=[
-            {'port' : '/dev/ttyUSBGPS' },
-            {'baud' : 115200 },
-            #{'baud' : 38400 },
-            {'frame_id' : 'gps_link' },
-            {'time_ref_source' : 'gps' },
-            {'use_GNSS_time' : False },
-            {'useRMC' : False }
-        ],
-        remappings=[("fix", "gps/fix")]
-    )
-
-    navsat_localizer = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'dual_ekf_navsat.launch.py'])
-        ),
-        launch_arguments={'use_sim_time': 'false', 'robot_model' : robot_model}.items()
-    )
-
-    drive_include = GroupAction(
-        actions=[
-            twist_mux,
-            delayed_controller_manager,
-            delayed_diff_drive_spawner,
-            delayed_joint_broad_spawner,
-            delayed_battery_state_broadcaster_spawner,
-            delayed_sonars_spawner
-        ]
-    )
-
-    sensors_include = GroupAction(
-        actions=[
-            ldlidar_node,
-            gps_node,
-            mpu9250driver_node
-        ]
-    )
-
-    localizers_include = GroupAction(
-        actions=[
-            LogInfo(msg='============ starting LOCALIZERS ==============='),
-            navsat_localizer,
-            # use either map_server OR slam_toolbox, as both are mappers
-            map_server,    # localization is left to GPS
-            #slam_toolbox, # localization via LIDAR
-        ]
-    )
-
-    delayed_loc = TimerAction(period=10.0, actions=[localizers_include])
-
-    delayed_nav = TimerAction(period=20.0, actions=[nav2])
 
     # Launch them all!
     return LaunchDescription([
@@ -298,12 +77,14 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace for dragger nodes'),
+
         robot_state_publisher,
-        # joystick,
         drive_include,
         sensors_include,
-        delayed_loc,
-        container_nav2,  # Add the container to the launch description, if 'use_composition': 'True' is set
-        delayed_nav
+        localizers_include,
+        navigation_include
     ])
-
---
+++ robots/dragger/launch/dragger.localizers.launch.py
@@ -0,0 +1,130 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, GroupAction, LogInfo
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+#
+# Generate launch description for Seggy robot localizers
+#
+# Localizers are SLAM and/or EKF nodes that provide robot pose estimates
+# to be used by navigation stack (Nav2) and other components.
+# They can be robot-specific, and that's why we have this separate launch file.
+#
+
+def generate_launch_description():
+
+    package_name='articubot_one'
+
+    # Accept namespace from parent launch or use empty default
+    namespace = LaunchConfiguration('namespace', default='')
+
+    # Keep interface compatible with being included from <robot_model>.launch.py
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    # Robot specific files reside under "robots" directory - sim, dragger, plucky, seggy, turtle...
+    robot_model = LaunchConfiguration('robot_model', default='')
+
+    # ==========================
+    # most used localizer - SLAM Toolbox
+    slam_toolbox_path = PathJoinSubstitution([
+        # see /opt/ros/jazzy/share/slam_toolbox/launch
+        FindPackageShare("slam_toolbox"), 'launch', 'online_async_launch.py'
+    ])
+    slam_toolbox_params_file = PathJoinSubstitution([
+        FindPackageShare(package_name), 'robots', robot_model, 'config', 'mapper_params.yaml'
+    ])
+
+    slam_toolbox = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(slam_toolbox_path),
+                launch_arguments={'use_sim_time': use_sim_time, 'namespace': namespace, 'slam_params_file': slam_toolbox_params_file}.items()
+    )
+    # ekf_localizer is needed for slam_toolbox, providing "a valid transform from your configured odom_frame to base_frame"
+    # also, produces odom_topic: /odometry/local which can be used by Nav2
+    # see https://github.com/SteveMacenski/slam_toolbox?tab=readme-ov-file#api
+    # see mapper_params.yaml
+    ekf_odom_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'ekf_odom.launch.py'
+    ])
+    ekf_localizer = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(ekf_odom_path),
+                launch_arguments={'use_sim_time': use_sim_time, 'robot_model' : robot_model, 'namespace': namespace}.items()
+    )
+
+    # ==========================
+    # optional alternative - Cartographer
+    cartographer_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'cartographer.launch.py'
+    ])
+    cartographer = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(cartographer_path),
+                launch_arguments={'use_sim_time': use_sim_time, 'namespace': namespace, 'robot_model': robot_model}.items()
+    )
+
+    # ==========================
+    # optional alternative - Map Server
+    # Map server is convenient when used with GPS and an empty map, for obstacle avoidance.
+    #map_yaml_file = PathJoinSubstitution([FindPackageShare(package_name), 'assets', 'maps', 'empty_map.yaml'])   # this is default anyway
+    map_yaml_file = '/opt/ros/jazzy/share/nav2_bringup/maps/warehouse.yaml'
+
+    map_server_path = PathJoinSubstitution([
+        FindPackageShare(package_name), 'launch', 'map_server.launch.py'
+    ])
+
+    map_server = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(map_server_path),
+                launch_arguments={'use_sim_time': use_sim_time, 'namespace': namespace}.items()       # empty_map - default
+                #launch_arguments={'map': map_yaml_file, 'use_sim_time': use_sim_time}.items() # warehouse
+    )
+
+    # ==========================
+    # debugging helper - rarely needed
+    # for experiments: a bad alternative to ekf_localizer for slam_toolbox - static transform publisher
+    tf_localizer = Node(package = "tf2_ros", 
+                    namespace=namespace,
+                    executable = "static_transform_publisher",
+                    arguments = ["0", "0", "0", "0", "0", "0", "odom", "base_link"]
+    )
+    
+    # ==========================
+
+    navsat_localizer = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([FindPackageShare(package_name), 'launch', 'dual_ekf_navsat.launch.py'])
+        ),
+        launch_arguments={'use_sim_time': 'false', 'robot_model' : robot_model}.items()
+    )
+
+    localizers_include = GroupAction(
+        actions=[
+            LogInfo(msg='============ starting LOCALIZERS ==============='),
+            navsat_localizer,
+            # use either map_server OR slam_toolbox, as both are mappers
+            map_server,    # localization is left to GPS
+            #slam_toolbox, # localization via LIDAR - use with navsat_localizer
+        ]
+    )
+
+    delayed_loc = TimerAction(period=10.0, actions=[localizers_include])
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='',
+            description='Namespace for drive nodes'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time if true'),
+
+        DeclareLaunchArgument(
+            'robot_model',
+            default_value='',
+            description='Robot model (e.g., seggy, plucky, dragger, turtle)'),
+
+        LogInfo(msg=['============ starting LOCALIZERS  namespace: "', namespace, '"  use_sim_time: ', use_sim_time, ', robot_model: ', robot_model]),
+
+        delayed_loc
+    ])
