f200d1274abaa7a0e1f230a266b4560f909f43a0
Sergei Grichine <msghub@hotmail.com>
2026-01-03 12:24:27 -0600

    Stingray sensors: assuming BNO055, RPLidar

--- robots/stingray/launch/stingray.sensors.launch.py
+++ robots/stingray/launch/stingray.sensors.launch.py
@@ -23,54 +23,17 @@ def generate_launch_description():
 
     # sensor nodes don't depend on robot_model and don't use package_name
 
-    # Lidar node - https://github.com/slgrobotics/robots_bringup/blob/main/Docs/Sensors/LD14.md
-    xv_11_driver_node = Node(
-        package='xv_11_driver',
-        namespace=namespace,
-        executable='xv_11_driver',
-        name='xv_11_driver',
-        output='screen',
-        respawn=True,
-        respawn_delay=4,
-        parameters=[{
-            'port': '/dev/ttyACM0',
-            'baud_rate': 115200,
-            'frame_id': 'laser_frame',
-            'firmware_version': 2,
-        }]
-    )
-
-    # IMU node - https://github.com/slgrobotics/robots_bringup/blob/main/Docs/Sensors/MPU9250.md
-    mpu9250_driver_node = Node(
-        package="mpu9250",
-        namespace=namespace,
-        executable="mpu9250",
-        name="mpu9250",
-        output='screen',
-        respawn=True,
-        respawn_delay=4,
-        emulate_tty=True,
-        parameters=[
-            {
-                #"print" : True,
-                "frequency" : 30,
-                "i2c_address" : 0x68,
-                "i2c_port" : 1,
-                "frame_id" : "imu_link",
-                "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
-                "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
-                "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
-                "magnetometer_scale": [1.0, 1.0, 1.0],
-                #"magnetometer_bias": [1.3345253592582676, 2.6689567513691685, -2.5294210260199957],
-                #"magnetometer_bias": [1.335, 4.0, 1.0],
-                #"magnetometer_bias": [1.335, 3.8, -2.5294210260199957],
-                "magnetometer_bias": [1.335, 4.0, -2.53],
-                "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
-                                        -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
-                                            0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
-            }
-        ],
-        remappings=[("imu", "imu/data")]
+    # Lidar node
+    rplidar_node = Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
     )
 
     bno055_driver_node = Node(
@@ -126,8 +89,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        xv_11_driver_node,
-        #mpu9250_driver_node,
+        rplidar_node,
         bno055_driver_node,
         ekf_imu_odom
     ])
