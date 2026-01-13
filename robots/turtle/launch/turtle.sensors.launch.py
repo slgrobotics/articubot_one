3436466fea88e5910073783bde343b5ef1a95f0b
Sergei Grichine <msghub@hotmail.com>
2026-01-12 19:16:45 -0600

    Update turtle.sensors.launch.py

--- robots/turtle/launch/turtle.sensors.launch.py
+++ robots/turtle/launch/turtle.sensors.launch.py
@@ -50,26 +50,31 @@ def generate_launch_description():
         respawn=True,
         respawn_delay=4,
         emulate_tty=True,
-        parameters=[
-            {
-                #"print" : True,
-                "frequency" : 30,
-                "i2c_address" : 0x68,
-                "i2c_port" : 1,
-                "frame_id" : "imu_link",
-                "acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
-                "acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
-                "gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
-                "magnetometer_scale": [1.0, 1.0, 1.0],
-                #"magnetometer_bias": [1.3345253592582676, 2.6689567513691685, -2.5294210260199957],
-                #"magnetometer_bias": [1.335, 4.0, 1.0],
-                #"magnetometer_bias": [1.335, 3.8, -2.5294210260199957],
-                "magnetometer_bias": [1.335, 4.0, -2.53],
-                "magnetometer_transform": [1.0246518952703103, -0.0240401565528902, 0.0030740476998857395,
-                                        -0.024040156552890175, 0.9926708357001245, 0.002288563295390304,
-                                            0.0030740476998857356, 0.0022885632953903268, 0.9837206150979054]
-            }
-        ],
+        parameters=[{
+                #"print": True,       # default False
+                #"raw_only": True,    # default False ("fusing" mode). When True - only publish raw IMU data - /imu/data_raw and /imu/mag
+                "frequency": 30,
+                "temp_pub_rate_hz": 1.0,  # temperature publish rate in Hz
+                "frame_id": "imu_link",
+                "i2c_address": 0x68,  # also, 0x0C shows up for built-in AK8963 magnetometer
+                "i2c_port": 1,        # a.k.a "bus". For Linux on Raspberry Pi Bus=1
+                "acceleration_scale": [1.0, 1.0, 1.0],  # small adjustment of scale factors for each axis, should be around 1.0
+                "acceleration_bias": [0.0, 0.0, 0.0],
+                "gyro_bias": [0.0, 0.0, 0.0],
+                "magnetometer_scale": [1.0, 1.0, 1.0],  # should be around 1.0
+                "magnetometer_bias": [0.0, 0.0, 0.0],
+                "magnetometer_transform": [
+                    1.0, 0.0, 0.0,
+                    0.0, 1.0, 0.0,
+                    0.0, 0.0, 1.0]
+                #"acceleration_scale": [1.0072387165748442, 1.0081436035838134, 0.9932769089604535],
+                #"acceleration_bias": [0.17038044467587418, 0.20464685207217453, -0.12461014438322202],
+                #"gyro_bias": [0.0069376404996494, -0.0619247665634732, 0.05717760948453845],
+                #"magnetometer_bias": [0.4533159894397744, 3.4555818146055564, -5.984038606178013],
+                #"magnetometer_transform": [   0.9983016121720226, 0.044890057238382707, 0.007231924972024632,
+                #                    0.044890057238382707, 1.2981683205953654, -0.1173361838042438, 
+                #                    0.007231924972024633, -0.11733618380424381, 0.7835617468652673]
+        }],
         remappings=[("imu", "imu/data")]
     )
 
@@ -123,13 +128,60 @@ def generate_launch_description():
             # Use "i2cdetect -y 1"
             "i2c_address": 0x68,
             "frame_id": "imu_link",
-            "pub_rate_hz": 100,      # integer, default 50
-            "temp_pub_rate_hz": 1.0, # float, default 1.0
+            "pub_rate_hz": 100,  # integer, default 50 in code, 200 here
+            "temp_pub_rate_hz": 1.0,     # float, default 1.0
+            "startup_calib_seconds": 5.0,     # default 3 seconds
+            "gyro_calib_max_std_dps": 2.0,    # warning threshold - if std dev is too high during calibration; default 1.0
+            "accel_calib_max_std_mps2": 0.35  # same for accel; default 0.35
+        }],
+    )
+
+    icm20948_driver_raw_node = Node(
+        package="ros2_icm20948",
+        executable="icm20948_raw_node",
+        name="icm20948_raw_node",
+        parameters=[{
+            # Note: for Linux on Raspberry Pi iBus=1 is hardcoded in linux_i2c.py
+            # SparkFun address is likely 0x69, generic GY-ICM20948 - 0x68
+            # Use "i2cdetect -y 1"
+            "i2c_address": 0x68,
+            "frame_id": "imu_link",
+            "pub_rate_hz": 100,  # integer, default 50 in code, 200 here
+            "temp_pub_rate_hz": 1.0,     # float, default 1.0
             "madgwick_beta": 0.08,
             "madgwick_use_mag": True,
-            "gyro_calib_seconds": 5.0, # default 3 seconds
-            "gyro_calib_max_std_dps": 1.0 # warning threshold - if std dev is too high during calibration
+            "startup_calib_seconds": 5.0,     # default 3 seconds
+            "gyro_calib_max_std_dps": 2.0,    # warning threshold - if std dev is too high during calibration; default 1.0
+            "accel_calib_max_std_mps2": 0.35  # same for accel; default 0.35
         }],
+    )
+
+    # Madgwick filter node to compute orientation quaternion *ONLY from raw IMU data* - do not run with "fusing" IMUs!
+    # publishes to "imu/data" topic
+    # https://github.com/CCNYRoboticsLab/imu_tools
+    # sudo apt install ros-${ROS_DISTRO}-imu-tools
+    madgwick_filter_node = Node(
+        package='imu_filter_madgwick',
+        executable='imu_filter_madgwick_node',
+        name='imu_filter',
+        output='screen',
+        parameters=[{
+            "stateless": False,
+            "use_mag": True,
+            "publish_tf": True,
+            "reverse_tf": False,
+            "fixed_frame": "odom",
+            "constant_dt": 0.0,
+            "publish_debug_topics": False,
+            "world_frame": "enu",
+            "gain": 0.1,
+            "zeta": 0.0,
+            "mag_bias_x": 0.0,
+            "mag_bias_y": 0.0,
+            "mag_bias_z": 0.0,
+            "orientation_stddev": 0.0
+        }],
+        #remappings=[("imu/mag", "imu/mag"), ("imu/data_raw", "imu/data_raw"), ("imu/data", "imu/data")],
     )
 
     # We need to run an EKF filter here to ensure its output stabilizes before starting SLAM Toolbox or other Localizers.
@@ -147,7 +199,9 @@ def generate_launch_description():
     return LaunchDescription([
         xv_11_driver_node,
         #mpu9250_driver_node,
-        bno055_driver_node,
-        #icm20948_driver_node,
+        #bno055_driver_node,
+        icm20948_driver_node,
+        #icm20948_driver_raw_node,
+        #madgwick_filter_node,
         ekf_imu_odom
     ])
