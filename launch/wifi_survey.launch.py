ab3721fd2e8cd20464ced444687c83f0d33aed88
Sergei Grichine <msghub@hotmail.com>
2026-07-12 16:36:07 -0500

    Update wifi_survey.launch.py

--- launch/wifi_survey.launch.py
+++ launch/wifi_survey.launch.py
@@ -1,6 +1,7 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import os
 
@@ -8,39 +9,48 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     package_name = 'wifi_logger_visualizer'
-    package_dir = get_package_share_directory(package_name)
-    config_filepath = os.path.join(package_dir, 'config', 'wifi_logger_config.yaml')
- 
-    ld.add_action(LogInfo(msg=["[wifi_survey.launch.py] ", " config_filepath: ", config_filepath]))
-  
-    # Declare launch arguments for nodes:
+    default_config_filepath = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'wifi_logger_config.yaml'
+    )
+
+    db_path = LaunchConfiguration('db_path')
+    config_filepath = LaunchConfiguration('config_filepath')
+
+    ld.add_action(LogInfo(msg=[
+        '[wifi_survey.launch.py] ',
+        'config_filepath: ', config_filepath,
+        ' db_path: ', db_path,
+    ]))
+
     db_path_arg = DeclareLaunchArgument(
         'db_path',
         default_value=os.path.join(os.getcwd(), 'wifi_data.db'),
         description='Path to the SQLite database file'
     )
-    
+
     config_filepath_arg = DeclareLaunchArgument(
         'config_filepath',
-        default_value=config_filepath,
+        default_value=default_config_filepath,
         description='Path to the configuration file'
     )
-    
+
     for action in [
         db_path_arg,
         config_filepath_arg,
     ]:
         ld.add_action(action)
-    
-    wifi_loger_node = Node(
+
+    wifi_logger_node = Node(
         package=package_name,
         executable='wifi_logger_node.py',
         name='wifi_logger_node',
-        parameters=[config_filepath],
+        parameters=[config_filepath, {'db_path': db_path}],
         # arguments=['--ros-args', '--log-level', 'DEBUG'],
         output='screen'
     )
-    ld.add_action(wifi_loger_node)
+    ld.add_action(wifi_logger_node)
 
     wifi_visualizer_node = Node(
         package=package_name,
@@ -50,9 +60,9 @@ def generate_launch_description():
         parameters=[config_filepath],
     )
     ld.add_action(wifi_visualizer_node)
-    
+
     heat_mapper_node = Node(
-        package='wifi_logger_visualizer',
+        package=package_name,
         executable='heat_mapper_node.py',
         name='heat_mapper_node',
         parameters=[config_filepath],
